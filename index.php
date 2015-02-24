8b161d3ba367a1e9bc5ad16dcfd8df022aec4b9a
Avinash <avinash@wohlig.com>
2015-02-24 17:11:57 +0530

    index change for cache

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");//Dont cache
+    header("Pragma: no-cache");//Dont cache
+    header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
